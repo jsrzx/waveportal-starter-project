76900085712631d6c6a5f53dee304b18c43d6942
rzexin <rzexin@gmail.com>
2023-01-10 18:30:33 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,29 +1,69 @@
-import * as React from "react";
-import { ethers } from "ethers";
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 
-export default function App() {
+const getEthereumObject = () => window.ethereum;
 
-  const wave = () => {
-    
-  }
-  
+const App = () => {
+  /*
+   * The passed callback function will be run when the page loads.
+   * More technically, when the App component "mounts".
+   */
+  useEffect(() => {
+    const ethereum = getEthereumObject();
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+  }, []);
+
   return (
     <div className="mainContainer">
-
       <div className="dataContainer">
         <div className="header">
-        👋 Hey there!
+          👋 Hey there!
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+          I am Farza and I worked on self-driving cars so that's pretty cool
+          right? Connect your Ethereum wallet and wave at me!
         </div>
 
-        <button className="waveButton" onClick={wave}>
+        <button className="waveButton" onClick={null}>
           Wave at Me
         </button>
       </div>
     </div>
   );
-}
+};
+
+export default App;
+
+// import * as React from "react";
+// import { ethers } from "ethers";
+// import './App.css';
+
+// export default function App() {
+
+//   const wave = () => {
+//   }
+//   return (
+//     <div className="mainContainer">
+
+//       <div className="dataContainer">
+//         <div className="header">
+//         👋 Hey there!
+//         </div>
+
+//         <div className="bio">
+//         I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+//         </div>
+
+//         <button className="waveButton" onClick={wave}>
+//           Wave at Me
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
+
